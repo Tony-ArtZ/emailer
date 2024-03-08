65354150f413668dcfbf46967d3c5029d3012a6d
BT08249 <adarshgku@gmail.com>
2024-03-08 22:18:21 +0530

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { sendEmail } from "./emailer.js";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+app.post("/", async (req, res) => {
+  try {
+    const body = req.body;
+    await sendEmail(body);
+    res.json({ message: "success" });
+  } catch (err) {
+    res.json({ error: err });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
